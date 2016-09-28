3d86f94ee8e185af039fc22867ba460bbd3d8fd1
Davíð Bachmann <davidba@novomaticls.com>
2016-09-28 10:26:46 +0000

    Fixed current temperature

--- src/WeatherImg.jsx
+++ src/WeatherImg.jsx
@@ -1,19 +1,31 @@
 import React, {Component} from 'react';
 import getIcon from './helpers/getIcon.js';
+import moment from 'moment';
 
 const WeatherImg = ({loading, img, data}) => {
 
   var style = {
     backgroundImage: `url(${img})`
   }
+  let bla = {};
 
+  if (!loading) {
+    for (var i = 0; i < data.results[0].forecast.length; i++) {
+      if(moment(data.results[0].forecast[i].ftime).format('LLLL').split(':')[0] === moment().format('LLLL').split(':')[0]) {
+        bla = {
+          degrees: data.results[0].forecast[i].T,
+          icon: data.results[0].forecast[i].W
+        }
+      }
+    }
+  }
   return (
     <div className="WeatherCard-img" style={style}>
       {loading ? ( <p>Loading</p> ) :
       (
         <p className="WeatherCard-caption">
-          <span className="WeatherCard-temp">{data.results[0].forecast[0].T}</span>
-          <i className={`WeatherCard-icon wi ${getIcon(data.results[0].forecast[0].W)}`}></i>
+          <span className="WeatherCard-temp">{bla.degrees}</span>
+          <i className={`WeatherCard-icon wi ${getIcon(bla.icon)}`}></i>
         </p>
       )
       }
